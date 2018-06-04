42f0893e410a5303861b67d1d2f9eb94de619716
ppm-tahi <tahi.balint@ppmas.no>
2018-06-04 21:23:03 +0200

    test

--- src/tests/test_myclass.py
+++ src/tests/test_myclass.py
@@ -3,12 +3,7 @@ Created on Jun 4, 2018
 
 @author: Felhasználó
 '''
-import sys
-import os
-
-sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'code'))
-
-from myclass import MyClass
+from src.code.myclass import MyClass
 
 class TestMyClass(object):
     def testA(self):
