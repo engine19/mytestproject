e6c620fe318e5f7bb509d77ad59d87e7fe91b8fb
ppm-tahi <tahi.balint@ppmas.no>
2018-06-04 22:32:58 +0200

    ....

---
+++ src/tests/test_myclass-1.py
@@ -0,0 +1,25 @@
+'''
+Created on Jun 4, 2018
+
+@author: Felhasználó
+'''
+
+from src.code.myclass import MyClass
+
+class TestMyClass2(object):
+    def testA(self):
+        mc = MyClass()
+        print("{0}".format(mc))
+        assert mc.PropA != 1
+        
+    def testB(self):
+        mc = MyClass()
+        mc.setPropA(1)
+        print("{0}".format(mc))
+        assert mc.PropA == 2
+        
+    def testC(self):
+        mc = MyClass()
+        mc.setPropA(3)
+        print("{0}".format(mc))
+        assert mc.PropA == 3
