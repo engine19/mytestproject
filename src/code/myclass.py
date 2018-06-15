f95209dd2d97eedb16492a2d366b14e60d1cfa19
ppm-tahi <tahi.balint@ppmas.no>
2018-06-15 13:51:16 +0200

    megcsinaltam kommitolsoasd.as.d..

--- src/code/myclass.py
+++ src/code/myclass.py
@@ -7,6 +7,14 @@ Created on Jun 4, 2018
 class MyClass(object):
     PropA = -1
     
+    def __init__(self, a,b,c):
+        self.a = a
+        self.b = b
+        self.c = c
+
     def setPropA(self, _propA):
         self.PropA = _propA
     
+
+myclass = MyClass(1,2,3)
+print(myclass.PropA)
